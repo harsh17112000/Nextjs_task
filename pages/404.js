d683904ba818a4b2c0202f47314c1f3ecba7ed1c
harsh17112000 <77334392+harsh17112000@users.noreply.github.com>
2022-02-07 18:28:09 +0530

    2files chnges

--- pages/404.js
+++ pages/404.js
@@ -1,20 +1,24 @@
 import React from 'react';
-import {useRouter} from "next/router"
+import { useRouter } from "next/router"
 
 const details = () => {
 
-   const history = useRouter();
+  const history = useRouter();
 
-   const redirect = ()=>{
-     history.push("/")
-   }
+  const redirect = () => {
+    history.push("/")
+  }
 
   return <>
-      <div className="container d-flex flex-column justify-content-center align-items-center">
-          <img src="./404.png" alt="404" style={{width:"400px"}} />
-          <p>Error page !</p>
-          <button className='btn btn-primary col-lg-6' onClick={()=>redirect()}>Home</button>
-      </div>
+    <div className="container d-flex flex-column justify-content-center align-items-center">
+      <img src="./404.png" alt="404" style={{ width: "400px" }} />
+      <h1>404</h1>
+      <h2>We are sorry, Page not Found!</h2>
+      <p>The Page your are looking for might have been removed had its name
+        changed and its temporarly unavailable
+      </p>
+      <button className='btn btn-primary col-lg-6' onClick={() => redirect()}>Home</button>
+    </div>
   </>;
 };
 
--- pages/detail.js
+++ pages/detail.js
@@ -1,6 +1,8 @@
 import React, { useEffect, useState } from 'react';
 import { useRouter } from "next/router"
 import Card from 'react-bootstrap/Card'
+import Modal from 'react-bootstrap/Modal'
+import Button from 'react-bootstrap/Button'
 import Avatar from '@mui/material/Avatar';
 import Styles from "../styles/signin.module.css"
 
@@ -9,12 +11,32 @@ const detail = () => {
     const [udata, setudata] = useState([]);
     // console.log(udata);
 
+
+    const [show, setShow] = useState(false);
+
+    const handleClose = () => setShow(false);
+    const handleShow = () => setShow(true);
+
+    var todayDate = new Date().toISOString().slice(0, 10);
+
     const getdata = () => {
         const data = localStorage.getItem("loginuserdata");
 
         if (data && data.length) {
             const checkeddata = JSON.parse(data)
             setudata(checkeddata[0]);
+
+            const birthday = checkeddata[0].map((e, k) => {
+                const j = e.date == todayDate
+                return j
+            });
+
+            if (birthday) {
+                setTimeout(() => {
+                    handleShow();
+                }, 3000)
+
+            }
         }
     };
     const history = useRouter();
@@ -23,7 +45,7 @@ const detail = () => {
         history.push("/")
     }
 
-    const logoutuser = ()=>{
+    const logoutuser = () => {
         localStorage.removeItem("loginuserdata");
         history.push("/");
     }
@@ -35,7 +57,7 @@ const detail = () => {
     return <>
         {
             udata && udata.length ? <div className="container mt-4">
-                <button className='btn btn-primary d-block' onClick={()=>logoutuser()}>Logout</button>
+                <button className='btn btn-primary d-block' onClick={() => logoutuser()}>Logout</button>
                 <div className=' d-flex justify-content-center align-items-center'>
                     <Card style={{ width: '18rem' }}>
                         <Card.Body>
@@ -56,11 +78,29 @@ const detail = () => {
                     </Card>
                 </div>
 
+
+                <Modal show={show} onHide={handleClose}>
+                    <Modal.Header closeButton>
+                        <Modal.Title>Hey, {udata[0].fname.toUpperCase()}</Modal.Title>
+                    </Modal.Header>
+                    <Modal.Body>🍰 Many Many Happy returns of the day ! </Modal.Body>
+                    <Modal.Footer>
+                        <Button variant="secondary" onClick={handleClose}>
+                            Close
+                        </Button>
+                        <Button variant="primary" onClick={handleClose}>
+                            Done
+                        </Button>
+                    </Modal.Footer>
+                </Modal>
             </div>
                 :
                 <div className="container d-flex flex-column justify-content-center align-items-center">
                     <img src="./404.png" alt="404" style={{ width: "400px" }} />
-                    <p>Error page !</p>
+                    <h2>We are sorry, Page not Found!</h2>
+                    <p>The Page your are looking for might have been removed had its name
+                        changed and its temporarly unavailable
+                    </p>
                     <button className='btn btn-primary col-lg-6' onClick={() => redirect()}>Home</button>
                 </div>
         }
@@ -68,3 +108,6 @@ const detail = () => {
 };
 
 export default detail;
+
+
+
